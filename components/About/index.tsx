54fb4a057717e712bda860d6e31c2e24bb984132
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-06-26 01:32:37 +0500

    new: about component

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,8 +1,11 @@
+import styles from './index.module.sass';
 const About = () => {
 	return (
-		<div className='about' style={{ background: 'url(./photo.jpg)' }}>
-			<div className='about__overlay'></div>
-			<h3 className='about__title'>About</h3>
+		<div className={styles.about} style={{ backgroundImage: 'url(./photo.jpg)' }}>
+			<div className={styles.about__overlay}></div>
+			<div className={styles.about__content}>
+				<h3 className='about__title'>About me</h3>
+			</div>
 		</div>
 	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 import Intro from '../components/Intro';
 import About from '../components/About';
-import styles from '../styles/home.module.sass';
 
 const IndexPage = () => {
 	return (
-		<div className={styles.home}>
+		<div className='home'>
 			<Intro />
 			<About />
 		</div>
