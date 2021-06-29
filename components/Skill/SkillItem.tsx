5d6fb5f10bbc7f2dda00f66325a3ce82ff73ffcb
dmitry-uknowme <45000651+dmitry-uknowme@users.noreply.github.com>
2021-06-29 22:22:53 +0500

    fix: skill item

--- components/Skill/SkillItem.tsx
+++ components/Skill/SkillItem.tsx
@@ -1,13 +1,64 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import useSectionObserver from '../../hooks/useSectionObserver';
 import styles from './index.module.sass';
 
-const SkillItem = () => {
+interface SkillItemProps {
+	value: number;
+	maxValue: number;
+	radius?: number;
+	fillAnimation?: any;
+}
+
+const SkillItem: React.FC<SkillItemProps> = ({ value, maxValue, radius, fillAnimation }) => {
+	const circleLength = 879.645943005142;
+	const skillColors = [
+		{ bgColor: 'rgba(255,0,0,0.1)', borderColor: '#C0392B' },
+		{ bgColor: 'rgba(240,116,39,0.1)', borderColor: '#D35400' },
+		{ bgColor: 'rgba(0,255,0,0.1)', borderColor: '#3FB14F' },
+	];
+	const [skillLevel, setSkillLevel] = useState(skillColors[0]);
+
+	// const getCircleLength = (radius: number | string): number => 2 * parseInt(radius) * Math.PI;
+	const currentSection = useSectionObserver();
+	useEffect(() => {
+		if (currentSection?.name === 'skill-list') {
+			fillAnimation(maxValue);
+		}
+	}, [currentSection]);
+
+	useEffect(() => {
+		const percent = 100 - (value / circleLength) * 100;
+		// console.log('pec', percent);
+		if (percent < 50) {
+			setSkillLevel(skillColors[0]);
+		}
+		if (percent >= 50 && percent < 80) {
+			setSkillLevel(skillColors[1]);
+		}
+		if (percent >= 80) {
+			setSkillLevel(skillColors[2]);
+		}
+	}, [value]);
+
 	return (
-		<svg className={styles.skillItem} viewBox='-10 -10 300 300'>
-			<circle className='progress-circle__range' r='140' cx='140' cy='140' fill='#8EE499' fillOpacity='0' stroke='#3FB14F' strokeWidth='20'></circle>
-			<g transform='translate(200,180)'>
-				<path className='progress-circle__arrow' stroke='#fff' fill='#fff' fillOpacity='1' strokeWidth='3' d='M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z'></path>
-			</g>
-		</svg>
+		<div className={styles.skillItem}>
+			<svg className='progress-circle' viewBox='-10 -10 300 300' width={100} height={100}>
+				<circle
+					className='progress-circle__range'
+					r='140'
+					cx='140'
+					cy='140'
+					fill={skillLevel.bgColor}
+					fillOpacity='1'
+					stroke={skillLevel.borderColor}
+					strokeWidth='20'
+					// onClick={(e) => console.log(e.target.getAttribute('stroke-dasharray'), e.target.r.baseVal.value)}
+					strokeDasharray={879.645943005142}
+					strokeDashoffset={value}></circle>
+			</svg>
+			<div className={styles.skillItem__value}>{(100 - (value / circleLength) * 100 - 1).toFixed(0)}</div>
+		</div>
 	);
 };
 
--- components/Skill/SkillList.tsx
+++ components/Skill/SkillList.tsx
@@ -1,15 +1,50 @@
+import { useState } from 'react';
+import useSectionObserver from '../../hooks/useSectionObserver';
 import styles from './index.module.sass';
 import SkillItem from './SkillItem';
 
-const skills = [1, 20, 50];
+interface ISkill {
+	value: number;
+	maxValue: number;
+}
 
 const SkillList = () => {
+	const circleLength = 879.645943005142;
+	const [skills, setSkills] = useState<ISkill[]>([
+		{ value: circleLength, maxValue: 80 },
+		{ value: circleLength, maxValue: 30 },
+		{ value: circleLength, maxValue: 50 },
+	]);
+	const runFillAnimation = (maxValue: number) => {
+		const maxPercent = () => {
+			if (maxValue >= 100) return 0;
+			return 879.645943005142 - maxValue * 8.79645943005142;
+		};
+		let fillTimer = setInterval(() => {
+			setSkills((state) => {
+				const currId = state.findIndex((skill) => skill.maxValue === maxValue);
+
+				if (state[currId].value <= maxPercent()) {
+					clearInterval(fillTimer);
+					return state;
+				}
+				return state.map((item, index) => {
+					if (index !== currId) return item;
+					return {
+						...item,
+						value: state[currId].value - 8.79645943005142,
+					};
+				});
+			});
+		}, 10);
+	};
+
 	return (
 		<section className={`skill-list ${styles.skillList}`}>
 			<h3 className={styles.skillList__title}>Skills</h3>
-			<div className={styles.skillList__items}>
-				{skills.map((skill) => (
-					<SkillItem />
+			<div className={styles.skillList__row}>
+				{skills.map(({ value, maxValue }) => (
+					<SkillItem value={value} maxValue={maxValue} radius={140} fillAnimation={runFillAnimation} />
 				))}
 			</div>
 		</section>
